9e8f7f5664cbb2c612576ebddef6abecc43448eb
junichitashiro <junichitashiro926@gmail.com>
2019-11-04 20:54:24 +0900

    自動ログインの作成

--- 自動ログイン.ps1
+++ 自動ログイン.ps1
@@ -1,1 +1,21 @@
-# PowerShell
+# 設定情報
+$url = "https://d4c-lt.com/contents/samplepage/login1.html"
+$id = "ID12345"
+$ps = "PW12345"
+
+# IEの起動から画面表示まで
+$ie = New-Object -ComObject InternetExplorer.Application
+$ie.Visible = $true
+$ie.Navigate($url)
+while($ie.Busy) { Start-Sleep -milliseconds 100 }
+$doc = $ie.document
+
+# ログイン情報の設定
+$doc.getElementById("user_name").value = $id
+$doc.getElementById("password").value = $ps
+$doc.getElementById("login_btn").click()
+# ------------------------------
+# 処理確認用
+# ------------------------------
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.MessageBox]::Show("ログインを実行","処理確認用")
