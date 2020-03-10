aa5a0749994b3a33133158613774f8ff39cda8eb
junichitashiro <junichitashiro926@gmail.com>
2020-03-10 22:11:14 +0900

    nullを比較演算子の左側に変更、タブ文字数を変更

--- タイムカード出力.ps1
+++ タイムカード出力.ps1
@@ -26,34 +26,34 @@ Write-Output $OutputText | Set-Content $filePath -Encoding Default
 $i = 1
 while ($i -le 31) {
 
-    # 要素"kubun_XX"のうち選択されている子要素のtextを取得する
-    $kubun = $doc.getElementsByName("kubun_" + $i)
-    $childNo = $kubun[0].selectedIndex
-    $outKubun = $kubun[0].children[$childNo].text
+  # 要素"kubun_XX"のうち選択されている子要素のtextを取得する
+  $kubun = $doc.getElementsByName("kubun_" + $i)
+  $childNo = $kubun[0].selectedIndex
+  $outKubun = $kubun[0].children[$childNo].text
 
-    # 開始時間の入力がなかったらデフォルト値を設定する
-    $startTime = $doc.getElementsByName("start")
-    if ($startTime[$i -1].value -eq $null) {
-        $outStartTime = "09:00"
-    }
-    else {
-        $outStartTime = $startTime[$i -1].value
-    }
+  # 開始時間の入力がなかったらデフォルト値を設定する
+  $startTime = $doc.getElementsByName("start")
+  if ($null -eq $startTime[$i -1].value) {
+    $outStartTime = "09:00"
+  }
+  else {
+    $outStartTime = $startTime[$i -1].value
+  }
 
-    # 終了時間の入力がなかったらデフォルト値を設定する
-    $endTime = $doc.getElementsByName("end")
-    if ($endTime[$i -1].value -eq $null) {
-        $outEndTime = "17:30"
-    }
-    else {
-        $outEndTime = $endTime[$i -1].value
-    }
+  # 終了時間の入力がなかったらデフォルト値を設定する
+  $endTime = $doc.getElementsByName("end")
+  if ($null -eq $endTime[$i -1].value) {
+    $outEndTime = "17:30"
+  }
+  else {
+    $outEndTime = $endTime[$i -1].value
+  }
 
-    # タブ区切りで内容を出力する
-    $OutputText = [String]$i + "`t" + $outKubun + "`t" + $outStartTime + "`t" + $outEndTime
-    Write-Output $OutputText | Add-Content $filePath -Encoding Default
+  # タブ区切りで内容を出力する
+  $OutputText = [String]$i + "`t" + $outKubun + "`t" + $outStartTime + "`t" + $outEndTime
+  Write-Output $OutputText | Add-Content $filePath -Encoding Default
 
-    $i += 1
+  $i += 1
 }
 
 # 完了メッセージ
