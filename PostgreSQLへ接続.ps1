7bd9b80b1a455d44f00ddf4fc304e541f2f937f8
junichitashiro <junichitashiro926@gmail.com>
2020-09-23 20:19:34 +0900

    PostgreSQLへの接続処理を新規作成

---
+++ PostgreSQLへ接続.ps1
@@ -0,0 +1,41 @@
+# --------------------------------------------------
+# DB接続パラメータの設定
+# --------------------------------------------------
+$serverName = "localhost"
+$portNo = "5432"
+$dbName = "postgres"
+$userName = "postgres"
+$password = "postgres"
+
+# --------------------------------------------------
+# DB接続処理の設定
+# --------------------------------------------------
+$dbConString = "Driver={PostgreSQL UNICODE};Server=$serverName;Port=$portNo;Database=$dbName;Uid=$userName;Pwd=$password;"
+$dbCon = New-Object System.Data.Odbc.OdbcConnection
+$dbCon.ConnectionString = $dbConString;
+$dbCon.Open()
+
+# --------------------------------------------------
+# SQLコマンドの作成
+# --------------------------------------------------
+$dbCmd = $dbCon.CreateCommand();
+$dbCmd.CommandText = "select * from test_table"
+
+# --------------------------------------------------
+# SQL実行結果をデータセットに格納する
+# --------------------------------------------------
+$dataAdp = New-Object -TypeName System.Data.Odbc.OdbcDataAdapter($dbCmd)
+$dataSet = New-Object -TypeName System.Data.DataSet
+# 実行結果を破棄する
+$dataAdp.Fill($dataSet) > $null
+
+# --------------------------------------------------
+# データセットをCSV出力する
+# --------------------------------------------------
+# Dドライブ直下に出力する
+$dataSet.Tables[0] | export-csv D:\export_csvFile.csv -notypeinformation -Encoding Default
+
+# --------------------------------------------------
+# DBコネクションを閉じる
+# --------------------------------------------------
+$dbCon.Close()
